318d632bc07de3dc52642e16773283ef95a8061f
fuxinpneg <fuxinpeng@aliyun.com>
2019-04-14 15:21:50 +0800

    解决冲突

--- src/main/java/com/example/demo/HelloWorld.java
+++ src/main/java/com/example/demo/HelloWorld.java
@@ -10,13 +10,8 @@ public class HelloWorld {
 	@ResponseBody
 	@RequestMapping("/helloworld")
 	public String helloWorld(){
-<<<<<<< HEAD
 		System.out.println("会当凌绝顶，");
 		System.out.println("一览众山小。");
-=======
-		System.out.println("沉舟侧畔千帆过，");
-		System.out.println("病树前头万木春。");
->>>>>>> branch 'master' of https://github.com/fuxinpeng/StudyGitHub.git
 		return "HelloWorld";
 	}
 	
